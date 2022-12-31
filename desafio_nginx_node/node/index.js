8484c65b2915495785c18e9c46ee327fadbd1666
Edgar Souza <edgarsouza@gmail.com>
2022-12-31 18:54:10 -0300

    Finalização

--- desafio_nginx_node/node/index.js
+++ desafio_nginx_node/node/index.js
@@ -1,21 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
-// const config = {
-//   host: 'db-node',
-//   user: 'root',
-//   password: 'root',
-//   database: 'nodedb'
-// };
-// const mysql = require('mysql');
-// const connection = mysql.createConnection(config);
+const config = {
+  host: 'db-node',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+};
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
 
-// const sql = `INSERT INTO people(name) values('Edgar')`
-// connection.query(sql)
 // connection.end()
 
-app.get('/', (req, res) => {
-  res.send('<h1>Full Cycle</h1>')
+app.get('/', async (req, res) => {
+  const connection = mysql.createConnection(config)
+  const sql = `INSERT INTO people(name) values('Edgar')`;
+  connection.query(sql)
+  connection.query('SELECT * FROM people', (err, response) => {
+    res.send(`<h1>Full Cycle Rocks - Desafio Nginx/Node.js</h1> <br> <h2>${response.map(user => `${user.name}`)}</h2>`)
+  });
+
+  connection.end()
 })
 
 app.listen(port, () => {
